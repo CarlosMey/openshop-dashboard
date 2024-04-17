21fde5b7ffbd1c4b400d5ad753541fb097caae9b
SistemasGeotop <sistemas.geotop@gmail.com>
2024-04-17 18:01:17 -0500

    commit employyes table

--- src/lib/helpers/index.jsx
+++ src/lib/helpers/index.jsx
@@ -38,3 +38,46 @@ export function getOrderStatus(status) {
 			)
 	}
 }
+
+
+
+export function getOrderEmployees(status) {
+	switch (status) {
+		case 'SENIOR':
+			return (
+				<span className="capitalize py-1 px-2 rounded-md text-xs text-yellow-600 bg-yellow-100">
+					{status.replaceAll('_', ' ').toLowerCase()}
+				</span>
+			)
+		case 'SEMI-JUNIOR':
+			return (
+				<span className="capitalize py-1 px-2 rounded-md text-xs text-green-600 bg-green-100">
+					{status.replaceAll('_', ' ').toLowerCase()}
+				</span>
+			)
+		case 'JUNIOR':
+			return (
+				<span className="capitalize py-1 px-2 rounded-md text-xs text-blue-600 bg-blue-100">
+					{status.replaceAll('_', ' ').toLowerCase()}
+				</span>
+			)
+		case 'PRACTICANTE':
+			return (
+				<span className="capitalize py-1 px-2 rounded-md text-xs text-red-600 bg-red-100">
+					{status.replaceAll('_', ' ').toLowerCase()}
+				</span>
+			)
+		case 'DELIVERED':
+			return (
+				<span className="capitalize py-1 px-2 rounded-md text-xs text-green-600 bg-green-100">
+					{status.replaceAll('_', ' ').toLowerCase()}
+				</span>
+			)
+		default:
+			return (
+				<span className="capitalize py-1 px-2 rounded-md text-xs text-gray-600 bg-gray-100">
+					{status.replaceAll('_', ' ').toLowerCase()}
+				</span>
+			)
+	}
+}
