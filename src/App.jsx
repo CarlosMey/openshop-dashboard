5b3aba344dc8108c280103476a170b9b988ca747
Carlos Mey <meycar96@gmail.com>
2024-04-16 23:46:19 -0500

    terminado el register

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/shared/Layout";
 import Dashboard from "./components/Dashboard";
 import Products from "./components/Products";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="products" element={<Products/>}/>
         </Route>
         <Route path="login" element={<Login/>}/>
+        <Route path="register" element={<Register/>}/>
       </Routes>
     </Router>
   );
