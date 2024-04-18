b45e840971db9802c4a10a3e5d62062bbd64984e
Freddy Morales <freddy.morales@usil.pe>
2024-04-18 15:50:30 -0500

    fix: register alignment

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useNavigate  } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from '../axios'
 import ValidationRegister from './validation/validationRegister'
 
@@ -17,20 +17,19 @@ export default function Register() {
         retypedPassword: '',
         username: '',
         firstName: '',
-        lastName: '',
+        lastName: ''
     })
 
-
     const [errors, setError] = useState({})
-    
-    const navigate = useNavigate();
 
-    function handleChange(e){
-        setValues({...values, [e.target.name]: e.target.value})
+    const navigate = useNavigate()
+
+    function handleChange(e) {
+        setValues({ ...values, [e.target.name]: e.target.value })
     }
 
     const onSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
         if (
             values.email !== '' &&
@@ -41,48 +40,47 @@ export default function Register() {
             values.lastName !== ''
         ) {
             try {
-            const request = {
-                username: values.username,
-                password: values.password,
-                retypedPassword: values.retypedPassword,
-                firstName: values.firstName,
-                lastName: values.lastName,
-            };
-            const { data } = await axios.post('users', { ...request });
-            console.log('Data: ', data);
-            navigate('/');
+                const request = {
+                    username: values.username,
+                    password: values.password,
+                    retypedPassword: values.retypedPassword,
+                    firstName: values.firstName,
+                    lastName: values.lastName
+                }
+                const { data } = await axios.post('users', { ...request })
+                console.log('Data: ', data)
+                navigate('/')
             } catch (error) {
-            console.log(error);
+                console.log(error)
             }
         }
-    };
+    }
 
     async function handleSubmit(e) {
-        e.preventDefault();
-        setError(ValidationRegister(values));
+        e.preventDefault()
+        setError(ValidationRegister(values))
 
-        await onSubmit(e);
+        await onSubmit(e)
     }
 
     return (
         <div>
-            <form onSubmit={ handleSubmit }>
-            <div className="h-screen text-gray-900 body-font">
+            <form onSubmit={handleSubmit}>
+                <div className="h-screen flex items-center text-gray-900 body-font">
                     <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
                         <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
                             <img src="https://djioficial.la/img/vooxell.png" alt="" />
                             <h1 className="title-font font-medium text-3xl  leading-relaxed">
-                            You have data, we give you a new dimension
+                                You have data, we give you a new dimension
                             </h1>
-
                         </div>
                         <div className="lg:w-2/6 md:w-1/2 border-2 border-green-600 bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
-                        <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Register</h2>
+                            <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Register</h2>
                             <div className="relative mb-4">
                                 <label htmlFor="email" className="leading-7 text-sm text-gray-900">
                                     Email
                                 </label>
-                                 <input
+                                <input
                                     type="email"
                                     value={values.email}
                                     onChange={handleChange}
@@ -90,7 +88,7 @@ export default function Register() {
                                     name="email"
                                     className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 />
-                                {errors.email && <p style={{color:"red", fontSize: "13px"}}>{errors.email }</p>}
+                                {errors.email && <p style={{ color: 'red', fontSize: '13px' }}>{errors.email}</p>}
                                 {/* <input
                                     type="email"
                                     value={email}
@@ -104,7 +102,7 @@ export default function Register() {
                                 <label htmlFor="password" className="leading-7 text-sm text-gray-900">
                                     Password
                                 </label>
-                                 <input
+                                <input
                                     type="password"
                                     value={values.password}
                                     onChange={handleChange}
@@ -112,7 +110,7 @@ export default function Register() {
                                     name="password"
                                     className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 />
-                                {errors.password && <p style={{color:"red", fontSize: "13px"}}>{errors.password }</p>}
+                                {errors.password && <p style={{ color: 'red', fontSize: '13px' }}>{errors.password}</p>}
                                 {/* <input
                                     type="password"
                                     value={password}
@@ -126,7 +124,7 @@ export default function Register() {
                                 <label htmlFor="retypedPassword" className="leading-7 text-sm text-gray-900">
                                     retypedPassword
                                 </label>
-                                 <input
+                                <input
                                     type="retypedPassword"
                                     value={values.retypedPassword}
                                     onChange={handleChange}
@@ -134,7 +132,9 @@ export default function Register() {
                                     name="retypedPassword"
                                     className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 />
-                                {errors.retypedPassword && <p style={{color:"red", fontSize: "13px"}}>{errors.retypedPassword }</p>}
+                                {errors.retypedPassword && (
+                                    <p style={{ color: 'red', fontSize: '13px' }}>{errors.retypedPassword}</p>
+                                )}
                                 {/* <input
                                     type="retypedPassword"
                                     value={retypedPassword}
@@ -148,7 +148,7 @@ export default function Register() {
                                 <label htmlFor="username" className="leading-7 text-sm text-gray-900">
                                     username
                                 </label>
-                                 <input
+                                <input
                                     type="username"
                                     value={values.username}
                                     onChange={handleChange}
@@ -156,7 +156,7 @@ export default function Register() {
                                     name="username"
                                     className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 />
-                                {errors.username && <p style={{color:"red", fontSize: "13px"}}>{errors.username }</p>}
+                                {errors.username && <p style={{ color: 'red', fontSize: '13px' }}>{errors.username}</p>}
                                 {/* <input
                                     type="username"
                                     value={username}
@@ -170,7 +170,7 @@ export default function Register() {
                                 <label htmlFor="firstName" className="leading-7 text-sm text-gray-900">
                                     firstName
                                 </label>
-                                 <input
+                                <input
                                     type="firstName"
                                     value={values.firstName}
                                     onChange={handleChange}
@@ -178,7 +178,9 @@ export default function Register() {
                                     name="firstName"
                                     className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 />
-                                {errors.firstName && <p style={{color:"red", fontSize: "13px"}}>{errors.firstName }</p>}
+                                {errors.firstName && (
+                                    <p style={{ color: 'red', fontSize: '13px' }}>{errors.firstName}</p>
+                                )}
                                 {/* <input
                                     type="firstName"
                                     value={firstName}
@@ -192,7 +194,7 @@ export default function Register() {
                                 <label htmlFor="lastName" className="leading-7 text-sm text-gray-900">
                                     lastName
                                 </label>
-                                 <input
+                                <input
                                     type="lastName"
                                     value={values.lastName}
                                     onChange={handleChange}
@@ -200,7 +202,7 @@ export default function Register() {
                                     name="lastName"
                                     className="w-full bg-gray-600 bg-opacity-20 focus:bg-transparent focus:ring-2 focus:ring-blue-900 rounded border border-gray-600 focus:border-blue-500 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                                 />
-                                {errors.lastName && <p style={{color:"red", fontSize: "13px"}}>{errors.lastName }</p>}
+                                {errors.lastName && <p style={{ color: 'red', fontSize: '13px' }}>{errors.lastName}</p>}
                                 {/* <input
                                     type="lastName"
                                     value={lastName}
